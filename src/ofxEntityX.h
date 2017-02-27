aef8f581ae20b666f3d1f0fa7eb5aca3792514e0
Martin Matilla <martin.m@coherentsynchro.com>
2017-02-27 11:14:03 +0100

    Fixed props not inheriting, and fixed typedef namespace issue, only using specific classes to avoid namespace conflicts

--- src/ofxEntityX.h
+++ src/ofxEntityX.h
@@ -3,7 +3,20 @@
 #include "entityx/entityx.h"
 #include "entityx/deps/Dependencies.h"
 
-using namespace entityx;
+using entityx::Component;
+using entityx::ComponentAddedEvent;
+using entityx::ComponentHandle;
+using entityx::ComponentRemovedEvent;
+using entityx::Entity;
+using entityx::EntityX;
+using entityx::EntityCreatedEvent;
+using entityx::EntityDestroyedEvent;
+using entityx::EntityManager;
+using entityx::Event;
+using entityx::EventManager;
+using entityx::System;
+using entityx::SystemManager;
+using entityx::TimeDelta;
 
 class ofxEntityX : public EntityX
 {};
